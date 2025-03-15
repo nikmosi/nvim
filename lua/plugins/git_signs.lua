8ed00d9ae6d6ed7026267776561bbe727dbff38f
nikmosi <i@nikmosi.ru>
2025-03-15 19:39:09 +0700

    setup gitsigns

--- lua/plugins/git_signs.lua
+++ lua/plugins/git_signs.lua
@@ -1,6 +1,9 @@
 return {
-    'lewis6991/gitsigns.nvim',
-    config = function()
-        require('gitsigns').setup()
-    end
+  "lewis6991/gitsigns.nvim",
+  config = function()
+    require "gitsigns".setup {
+      signcolumn = true, -- Убираем знаки слева
+      numhl = false,     -- Включаем подсветку номеров строк
+    }
+  end
 }
