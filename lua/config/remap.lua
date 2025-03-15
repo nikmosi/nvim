f9b9e61307ffb81232e07bdc0bee5c2816aaff90
nikmosi <i@nikmosi.ru>
2025-03-15 17:30:53 +0700

    add close all buffs except current

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,3 +1,13 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set('n', '<leader>bc', function()
+  local current_buf = vim.api.nvim_get_current_buf()
+  local bufs = vim.api.nvim_list_bufs()
+
+  for _, buf in ipairs(bufs) do
+    if buf ~= current_buf then
+      vim.api.nvim_buf_delete(buf, { force = true })
+    end
+  end
+end, { noremap = true, silent = true })
 vim.keymap.set("i", "jk", "<Esc>")
