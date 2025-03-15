d2196c394b55724c9f680f17baa73b6e60ba1015
nikmosi <i@nikmosi.ru>
2025-03-15 19:31:39 +0700

    setup diagnostic

---
+++ lua/config/diagnostic.lua
@@ -0,0 +1,14 @@
+vim.diagnostic.config {
+  signs = true,
+  virtual_text = true,           -- Отключить текст диагностики в самом коде
+  float = { border = "rounded" }, -- Окно всплывающей подсказки с границей
+  underline = true,
+  update_in_insert = false,
+}
+
+-- Настройка знаков диагностики
+local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
