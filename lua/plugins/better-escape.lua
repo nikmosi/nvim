d53ba50258246177e0f6c846246974ea9770bcf4
nikmosi <i@nikmosi.ru>
2025-03-16 20:31:20 +0700

    change to jk bind

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -1,6 +1,14 @@
 return {
   "max397574/better-escape.nvim",
-  config = function()
-    require "better_escape".setup()
-  end,
+  opts = {
+    timeout = 200,
+    default_mappings = false,
+    mappings = {
+      i = {
+        j = {
+          k = "<Esc>"
+        }
+      }
+    }
+  },
 }
