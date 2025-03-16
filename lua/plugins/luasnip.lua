b4f080ddb47075f337f96b904cec17557748b46c
nikmosi <i@nikmosi.ru>
2025-03-16 23:08:24 +0700

    setup luasnip

---
+++ lua/plugins/luasnip.lua
@@ -0,0 +1,13 @@
+return {
+  "L3MON4D3/LuaSnip",
+  config = function()
+    local luasnip = require "luasnip"
+
+    luasnip.config.set_config {
+      history = true,
+      updateevents = "TextChanged,TextChangedI",
+    }
+
+    require "luasnip.loaders.from_vscode".lazy_load()
+  end
+}
