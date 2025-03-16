f4013c56b7bd35a0bb44d8962820120c27f9f5e3
nikmosi <i@nikmosi.ru>
2025-03-16 11:16:18 +0700

    add new themes

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,6 @@
+return {
+  { "folke/tokyonight.nvim", priority = 1000, },
+  { "catppuccin/nvim",       name = "catppuccin", priority = 1000 },
+  { "rose-pine/neovim",      name = "rose-pine", },
+  { "EdenEast/nightfox.nvim" },
+}
