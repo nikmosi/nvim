85b7b1bfcabdfd439722a7fed42e0f7eb836f76a
nikmosi <i@nikmosi.ru>
2025-03-20 17:43:05 +0700

    fix(lsp): remove binds using in snacks.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -99,11 +99,11 @@ return {
         local opts = { buffer = event.buf }
 
         vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        -- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        -- vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
         vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
         vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        -- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
         vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
         vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts)
         vim.keymap.set("n", "<leader>ld", function()
