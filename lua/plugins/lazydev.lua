52e1aa1e781628422ba2d58776aa7cbfde7ffb7f
nikmosi <i@nikmosi.ru>
2025-03-20 17:52:15 +0700

    fix(lazydev): delete duplicate

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -7,25 +7,18 @@ return {
       "snacks.nvim",
       -- It can also be a table with trigger words / mods
       -- Only load luvit types when the `vim.uv` word is found
-      { path = "${3rd}/luv/library",        words = { "vim%.uv" } },
+      { path = "${3rd}/luv/library", words = { "vim%.uv" } },
       -- always load the LazyVim library
       "LazyVim",
       -- Only load the lazyvim library when the `LazyVim` global is found
-      { path = "LazyVim",                   words = { "LazyVim" } },
-      -- Load the wezterm types when the `wezterm` module is required
-      -- Needs `justinsgithub/wezterm-types` to be installed
-      { path = "wezterm-types",             mods = { "wezterm" } },
-      -- Load the xmake types when opening file named `xmake.lua`
-      -- Needs `LelouchHe/xmake-luals-addon` to be installed
-      { path = "xmake-luals-addon/library", files = { "xmake.lua" } },
+      { path = "LazyVim",            words = { "LazyVim" } },
     },
     -- always enable unless `vim.g.lazydev_enabled = false`
     -- This is the default
     enabled = function(root_dir)
-      return vim.g.lazydev_enabled == nil and true or vim.g.lazydev_enabled
-    end,
-    -- disable when a .luarc.json file is found
-    enabled = function(root_dir)
+      if vim.g.lazydev_enabled == false then
+        return false
+      end
       return not vim.uv.fs_stat(root_dir .. "/.luarc.json")
     end,
   },
