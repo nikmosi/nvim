9ad5fc90dcb7e8a3d13986015571497b133e60a3
nikmosi <i@nikmosi.ru>
2025-03-21 13:16:46 +0700

    feat(dashboard): swap recent_files and projects

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -12,8 +12,8 @@ return {
       sections = {
         { section = "header" },
         { section = "keys", gap = 1, padding = 1 },
-        { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
         { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
         {
           pane = 2,
           icon = " ",
