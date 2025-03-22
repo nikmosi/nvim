f520e67d88bbc627dce1fc374121f71f7e5b6612
nikmosi <i@nikmosi.ru>
2025-03-22 22:14:53 +0700

    refactor(python): remove unused

--- lua/config/lsp/pyright.lua
+++ lua/config/lsp/pyright.lua
@@ -14,7 +14,5 @@ return {
     local py = require "lang.python"
     py.env(new_root_dir)
     new_config.settings.python.pythonPath = vim.fn.exepath "python"
-    -- new_config.cmd_env.PATH = py.env(new_root_dir) .. new_config.cmd_env.PATH
-    vim.notify(vim.inspect(py.pep582(new_root_dir)))
   end,
 }
