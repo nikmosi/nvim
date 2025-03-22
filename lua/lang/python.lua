e72f8319d92c442d8f25f756a3ee15fb070f10d8
nikmosi <i@nikmosi.ru>
2025-03-22 22:04:18 +0700

    refactor(lang/python): remove deprecated function

--- lua/lang/python.lua
+++ lua/lang/python.lua
@@ -5,7 +5,7 @@ local path = require("lspconfig.util").path
 local function get_python_dir(root_dir)
   -- Ищем .venv в корне проекта
   local venv_path = vim.fs.joinpath(root_dir, ".venv")
-  local stat = vim.loop.fs_stat(venv_path)
+  local stat = vim.uv.fs_stat(venv_path)
   if stat and stat.type == "directory" then
     return venv_path
   end
@@ -13,10 +13,10 @@ local function get_python_dir(root_dir)
   -- Если .venv не найден в корне, ищем в поддиректориях
   for _, dir in ipairs(vim.fn.readdir(root_dir)) do
     local subdir = vim.fs.joinpath(root_dir, dir)
-    local subdir_stat = vim.loop.fs_stat(subdir)
+    local subdir_stat = vim.uv.fs_stat(subdir)
     if subdir_stat and subdir_stat.type == "directory" then
       local sub_venv = vim.fs.joinpath(subdir, ".venv")
-      local sub_venv_stat = vim.loop.fs_stat(sub_venv)
+      local sub_venv_stat = vim.uv.fs_stat(sub_venv)
       if sub_venv_stat and sub_venv_stat.type == "directory" then
         return sub_venv
       end
@@ -27,15 +27,16 @@ local function get_python_dir(root_dir)
 end
 
 local _virtual_env = ""
-local _package = ""
 
-local function py_bin_dir()
-  return path.join(_virtual_env, "bin:")
-end
+local function py_bin_dir() return path.join(_virtual_env, "bin:") end
 
 M.env = function(root_dir)
   if not vim.env.VIRTUAL_ENV or vim.env.VIRTUAL_ENV == "" then
-    _virtual_env = get_python_dir(root_dir)
+    local venv = get_python_dir(root_dir)
+    if not venv then
+      return
+    end
+    _virtual_env = venv
     vim.notify("using venv: " .. _virtual_env)
   end
 
