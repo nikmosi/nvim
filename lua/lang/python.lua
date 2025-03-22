62e53903d59869af6abb2a2bdbb20c58a139f687
nikmosi <i@nikmosi.ru>
2025-03-22 22:15:13 +0700

    refactor(python): remove useless ifs

--- lua/lang/python.lua
+++ lua/lang/python.lua
@@ -26,31 +26,27 @@ local function get_python_dir(root_dir)
   return nil
 end
 
-local _virtual_env = ""
-
-local function py_bin_dir() return path.join(_virtual_env, "bin:") end
+local function py_bin_dir(env) return path.join(env, "bin:") end
 
 M.env = function(root_dir)
-  if not vim.env.VIRTUAL_ENV or vim.env.VIRTUAL_ENV == "" then
-    local venv = get_python_dir(root_dir)
-    if not venv then
-      return
-    end
-    _virtual_env = venv
-    vim.notify("using venv: " .. _virtual_env)
+  if vim.env.VIRTUAL_ENV and vim.env.VIRTUAL_ENV ~= "" then
+    return
   end
 
-  if _virtual_env ~= "" then
-    vim.env.VIRTUAL_ENV = _virtual_env
-    vim.env.PATH = py_bin_dir() .. vim.env.PATH
+  local _virtual_env = get_python_dir(root_dir)
+  if not _virtual_env then
+    return
   end
 
-  if _virtual_env ~= "" and vim.env.PYTHONHOME then
+  vim.notify("using venv: " .. _virtual_env)
+
+  vim.env.VIRTUAL_ENV = _virtual_env
+  vim.env.PATH = py_bin_dir(_virtual_env) .. vim.env.PATH
+
+  if vim.env.PYTHONHOME then
     vim.env.old_PYTHONHOME = vim.env.PYTHONHOME
     vim.env.PYTHONHOME = ""
   end
-
-  return _virtual_env ~= "" and py_bin_dir() or ""
 end
 
 return M
