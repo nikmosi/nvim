857e981eceb9b047fa1c273d59802c5250c4fffb
nikmosi <i@nikmosi.ru>
2025-03-23 22:10:02 +0700

    fix(remap): change p to P

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -3,4 +3,4 @@ vim.g.mapleader = " "
 -- Move between buffers using keys
 vim.keymap.set("n", "L", vim.cmd.bnext, { noremap = true, silent = true, desc = "Next buffer" })
 vim.keymap.set("n", "H", vim.cmd.bprev, { noremap = true, silent = true, desc = "Previous buffer" })
-vim.keymap.set("x", "<leader>p", "\"_dp", { noremap = true, silent = true, desc = "Delete to void" })
+vim.keymap.set("x", "<leader>p", "\"_dP", { noremap = true, silent = true, desc = "Delete to void" })
