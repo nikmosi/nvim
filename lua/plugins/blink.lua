1d13b08d2caccc9453ab91daa6068fd423049706
nikmosi <i@nikmosi.ru>
2025-03-23 15:56:38 +0700

    feat(cmp): enable signature

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -53,7 +53,7 @@ return {
     },
 
     fuzzy = { implementation = "prefer_rust_with_warning" },
-    signature = { window = { border = "single" } },
+    signature = { enabled = true, window = { border = "single" } },
     completion = {
       documentation = { auto_show = true, auto_show_delay_ms = 500, window = { border = "single" } },
       ghost_text = { enabled = true },
