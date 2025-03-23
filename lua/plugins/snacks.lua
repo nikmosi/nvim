4fc58351495b73b01f2548b6a406ec161aec22a0
nikmosi <i@nikmosi.ru>
2025-03-23 17:37:09 +0700

    feat(snacks): change vim.ui.input with Snacks.input

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -162,6 +162,7 @@ return {
         _G.dd = function(...) Snacks.debug.inspect(...) end
         _G.bt = function() Snacks.debug.backtrace() end
         vim.print = _G.dd -- Override print to use snacks for `:=` command
+        vim.ui.input = Snacks.input
 
         -- Create some toggle mappings
         Snacks.toggle.option("spell", { name = "Spelling" }):map "<leader>us"
