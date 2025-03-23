babc223324bf311d924bbcecdd5643b685804ea6
nikmosi <i@nikmosi.ru>
2025-03-23 21:57:12 +0700

    refactor(remap): add desc to L and H

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 
 -- Move between buffers using keys
-vim.keymap.set("n", "L", vim.cmd.bnext, { noremap = true, silent = true }) -- Next buffer
-vim.keymap.set("n", "H", vim.cmd.bprev, { noremap = true, silent = true }) -- Previous buffer
+vim.keymap.set("n", "L", vim.cmd.bnext, { noremap = true, silent = true, desc = "Next buffer" })
+vim.keymap.set("n", "H", vim.cmd.bprev, { noremap = true, silent = true, desc = "Previous buffer" })
 vim.keymap.set("x", "<leader>p", "\"_dp", { noremap = true, silent = true, desc = "Delete to void" })
