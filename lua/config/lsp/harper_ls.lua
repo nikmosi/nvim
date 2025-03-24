9bc9a081de517b9535516090da1eee56ef5c9b98
nikmosi <i@nikmosi.ru>
2025-03-25 00:42:36 +0700

    chore(lsp): add filetypes to harper_ls

--- lua/config/lsp/harper_ls.lua
+++ lua/config/lsp/harper_ls.lua
@@ -1,4 +1,34 @@
 return {
+  filetypes = {
+    "c",
+    "cmake",
+    "cpp",
+    "csharp",
+    "dart",
+    "git",
+    "go",
+    "haskell",
+    "html",
+    "java",
+    "javascript",
+    "javascriptreact",
+    "literate haskell",
+    "lhaskell",
+    "markdown",
+    "nix",
+    "php",
+    "plaintext",
+    "python",
+    "ruby",
+    "rust",
+    "shellscript",
+    "swift",
+    "toml",
+    "typescript",
+    "typescriptreact",
+    "typst",
+  },
+
   settings = {
     ["harper-ls"] = {
       linters = {
