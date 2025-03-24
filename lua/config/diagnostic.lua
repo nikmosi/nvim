f38b3d13813858021e2056478c92f237a0c59d23
nikmosi <i@nikmosi.ru>
2025-03-25 01:17:10 +0700

    chore(diagnostic): change diagnostic settings

--- lua/config/diagnostic.lua
+++ lua/config/diagnostic.lua
@@ -1,3 +1,5 @@
+local colors = require("tokyonight.colors").setup()
+
 vim.diagnostic.config {
   signs = true,
   virtual_text = true, -- Отключить текст диагностики в самом коде
@@ -6,9 +8,27 @@ vim.diagnostic.config {
   update_in_insert = false,
 }
 
--- Настройка знаков диагностики
-local signs = { Error = "", Warn = "", Hint = "", Info = "" }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+local symbols = { Error = "󰅙", Info = "󰋼", Hint = "󰌵", Warn = "" }
+
+for name, icon in pairs(symbols) do
+  local hl = "DiagnosticSign" .. name
+  vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
 end
+
+-- Настройка highlight групп для разных типов диагностики
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { undercurl = true, sp = colors.red })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", { underline = true, sp = colors.orange })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineInfo", { underline = true, sp = colors.blue })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", { underline = true, sp = colors.green })
+
+-- Настройка цветов для значков (signs)
+vim.api.nvim_set_hl(0, "DiagnosticSignError", { fg = colors.red })
+vim.api.nvim_set_hl(0, "DiagnosticSignWarn", { fg = colors.orange })
+vim.api.nvim_set_hl(0, "DiagnosticSignInfo", { fg = colors.blue })
+vim.api.nvim_set_hl(0, "DiagnosticSignHint", { fg = colors.green })
+
+-- Настройка цветов для всплывающих окон
+vim.api.nvim_set_hl(0, "DiagnosticFloatingError", { fg = colors.red })
+vim.api.nvim_set_hl(0, "DiagnosticFloatingWarn", { fg = colors.orange })
+vim.api.nvim_set_hl(0, "DiagnosticFloatingInfo", { fg = colors.blue })
+vim.api.nvim_set_hl(0, "DiagnosticFloatingHint", { fg = colors.green })
