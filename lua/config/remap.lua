b8d40b9cc0d8cde23d21ec2c91f93d66b74c65c9
nikmosi <i@nikmosi.ru>
2025-03-27 02:51:40 +0700

    fix(remap): fix paste in instert mode

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -5,4 +5,6 @@ vim.keymap.set("n", "L", vim.cmd.bnext, { noremap = true, silent = true, desc = 
 vim.keymap.set("n", "H", vim.cmd.bprev, { noremap = true, silent = true, desc = "Previous buffer" })
 vim.keymap.set("x", "<leader>p", "\"_dP", { noremap = true, silent = true, desc = "Delete to void" })
 vim.keymap.set("n", "<leader>v", "\"+p", { noremap = true, silent = true, desc = "Paste from system clip" })
+vim.keymap.set({ "n", "v" }, "<C-V>", "\"+p", { noremap = true, silent = true, desc = "Paste from system clip" })
+vim.keymap.set("i", "<C-V>", "<esc>\"+pa", { noremap = true, silent = true, desc = "Paste from system clip" })
 vim.keymap.set("n", "<leader>V", "\"+p", { noremap = true, silent = true, desc = "Paste from system clip" })
