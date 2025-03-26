f0fbfd8b50c3fb4212dcaec2ef5c3aac27626c30
nikmosi <i@nikmosi.ru>
2025-03-27 02:51:55 +0700

    chore(remap): add qol maps

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -8,3 +8,11 @@ vim.keymap.set("n", "<leader>v", "\"+p", { noremap = true, silent = true, desc =
 vim.keymap.set({ "n", "v" }, "<C-V>", "\"+p", { noremap = true, silent = true, desc = "Paste from system clip" })
 vim.keymap.set("i", "<C-V>", "<esc>\"+pa", { noremap = true, silent = true, desc = "Paste from system clip" })
 vim.keymap.set("n", "<leader>V", "\"+p", { noremap = true, silent = true, desc = "Paste from system clip" })
+
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<A-j>", "V:m '>+1<CR>gv=gv<Esc>")
+vim.keymap.set("n", "<A-k>", "V:m '<-2<CR>gv=gv<Esc>")
+
+vim.keymap.set("n", "<CR>", [[{-> v:hlsearch ? ":nohl\<CR>" : "\<CR>"}()]], { silent = true, expr = true })
