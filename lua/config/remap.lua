44cec512a9439fb1e763898f85539aa4051a4ba8
nikmosi <i@nikmosi.ru>
2025-03-27 02:17:24 +0700

    feat(remap): add keymaps to paste from system clip

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -4,3 +4,5 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "L", vim.cmd.bnext, { noremap = true, silent = true, desc = "Next buffer" })
 vim.keymap.set("n", "H", vim.cmd.bprev, { noremap = true, silent = true, desc = "Previous buffer" })
 vim.keymap.set("x", "<leader>p", "\"_dP", { noremap = true, silent = true, desc = "Delete to void" })
+vim.keymap.set("n", "<leader>v", "\"+p", { noremap = true, silent = true, desc = "Paste from system clip" })
+vim.keymap.set("n", "<leader>V", "\"+p", { noremap = true, silent = true, desc = "Paste from system clip" })
