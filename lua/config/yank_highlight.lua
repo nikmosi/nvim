d4fbd951cde006224972951341a806c14d549f0c
nikmosi <i@nikmosi.ru>
2025-03-27 02:31:26 +0700

    chore(yank_highlight): change timeout

--- lua/config/yank_highlight.lua
+++ lua/config/yank_highlight.lua
@@ -2,5 +2,5 @@ vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
   group = "YankHighlight",
   pattern = "*",
-  callback = function() vim.highlight.on_yank { higroup = "IncSearch", timeout = 100 } end,
+  callback = function() vim.highlight.on_yank { higroup = "IncSearch", timeout = 150 } end,
 })
