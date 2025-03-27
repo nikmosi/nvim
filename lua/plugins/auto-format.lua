528836c396c390e5443024175e4e87c1dec0ff9f
nikmosi <i@nikmosi.ru>
2025-03-27 11:17:08 +0700

    feat(format): change plugin to conform

--- lua/plugins/auto-format.lua
+++ lua/plugins/auto-format.lua
@@ -1,43 +1,19 @@
 return {
-  "lukas-reineke/lsp-format.nvim",
-  event = "VimEnter",
-  config = function()
-    vim.api.nvim_create_autocmd("LspAttach", {
-      desc = "Auto format",
-      callback = function(event)
-        local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client then
-          require("lsp-format").on_attach(client, event.bufnr)
-        else
-          vim.notify("LSP client not found for buffer " .. event.bufnr, vim.log.levels.WARN)
-        end
-      end,
-    })
-
-    vim.api.nvim_create_autocmd("BufWritePost", {
-      pattern = "*.lua",
-      callback = function()
-        local bufnr = vim.api.nvim_get_current_buf()
-        local clients = vim.lsp.get_clients { bufnr = bufnr }
-        local formatted = false
-
-        for _, client in ipairs(clients) do
-          if client.name == "lua_ls" then
-            local win_id = vim.api.nvim_get_current_win()
-            vim.system({ "stylua", "%", vim.api.nvim_buf_get_name(bufnr) }, nil, function()
-              vim.schedule(function()
-                if vim.api.nvim_win_is_valid(win_id) then
-                  vim.cmd "edit"
-                end
-              end)
-            end)
-            formatted = true
-          end
-        end
-        if not formatted then
-          vim.buf.lsp.format { async = true }
-        end
-      end,
-    })
-  end,
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    ---@module "conform"
+    ---@type conform.setupOpts
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "ruff_fix", "ruff_format", "ruff_organize_import" },
+      },
+      default_format_opts = {
+        lsp_format = "fallback",
+      },
+      format_on_save = { timeout_ms = 500 },
+    },
+  },
 }
