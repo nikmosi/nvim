bb78c92ae17246ff206a59e93601181514a12580
nikmosi <i@nikmosi.ru>
2025-04-10 01:24:28 +0700

    chore(git_signs): remove comments

--- lua/plugins/git_signs.lua
+++ lua/plugins/git_signs.lua
@@ -2,7 +2,7 @@ return {
   "lewis6991/gitsigns.nvim",
   event = "BufRead",
   opts = {
-    signcolumn = true, -- Убираем знаки слева
-    numhl = false, -- Включаем подсветку номеров строк
+    signcolumn = true,
+    numhl = false,
   },
 }
