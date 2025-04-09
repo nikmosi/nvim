c71c2790894cb56bb10ec85c04ecbdea236bc078
nikmosi <i@nikmosi.ru>
2025-04-10 01:15:32 +0700

    chore(blink): improve settings

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,7 +1,12 @@
 return {
   "saghen/blink.cmp",
   event = "BufRead",
-  dependencies = { "rafamadriz/friendly-snippets", "echasnovski/mini.icons" },
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+    "echasnovski/mini.icons",
+    "Kaiser-Yang/blink-cmp-git",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
 
   version = "*",
   ---@module 'blink.cmp'
@@ -29,9 +34,15 @@ return {
       default = { "lsp", "path", "snippets", "buffer" },
 
       providers = {
+        lazydev = {
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+          -- make lazydev completions top priority (see `:h blink.cmp`)
+          score_offset = 100,
+        },
         lsp = {
-          name = "LSP",
-          module = "blink.cmp.sources.lsp",
+          name = "SNIPPETS",
+          module = "blink.cmp.sources.snippets",
           opts = {}, -- Passed to the source directly, varies by source
 
           --- NOTE: All of these options may be functions to get dynamic behavior
@@ -46,8 +57,70 @@ return {
           -- If this provider returns 0 items, it will fallback to these providers.
           -- If multiple providers fallback to the same provider, all of the providers must return 0 items for it to fallback
           fallbacks = {},
-          score_offset = -7, -- Boost/penalize the score of the items
+          score_offset = 10, -- Boost/penalize the score of the items
           override = nil, -- Override the source's functions
+        },
+
+        git = {
+          module = "blink-cmp-git",
+          name = "Git",
+          -- only enable this source when filetype is gitcommit, markdown, or 'octo'
+          enabled = function() return vim.tbl_contains({ "octo", "gitcommit", "markdown" }, vim.bo.filetype) end,
+          --- @module 'blink-cmp-git'
+          --- @type blink-cmp-git.Options
+          opts = {
+            commit = {
+              -- You may want to customize when it should be enabled
+              -- The default will enable this when `git` is found and `cwd` is in a git repository
+              -- enable = function() end
+              -- You may want to change the triggers
+              -- triggers = { ':' },
+            },
+            git_centers = {
+              github = {
+                -- Those below have the same fields with `commit`
+                -- Those features will be enabled when `git` and `gh` (or `curl`) are found and
+                -- remote contains `github.com`
+                -- issue = {
+                --     get_token = function() return '' end,
+                -- },
+                -- pull_request = {
+                --     get_token = function() return '' end,
+                -- },
+                -- mention = {
+                --     get_token = function() return '' end,
+                --     get_documentation = function(item)
+                --         local default = require('blink-cmp-git.default.github')
+                --             .mention.get_documentation(item)
+                --         default.get_token = function() return '' end
+                --         return default
+                --     end
+                -- }
+              },
+              gitlab = {
+                -- Those below have the same fields with `commit`
+                -- Those features will be enabled when `git` and `glab` (or `curl`) are found and
+                -- remote contains `gitlab.com`
+                -- issue = {
+                --     get_token = function() return '' end,
+                -- },
+                -- NOTE:
+                -- Even for `gitlab`, you should use `pull_request` rather than`merge_request`
+                -- pull_request = {
+                --     get_token = function() return '' end,
+                -- },
+                -- mention = {
+                --     get_token = function() return '' end,
+                --     get_documentation = function(item)
+                --         local default = require('blink-cmp-git.default.gitlab')
+                --            .mention.get_documentation(item)
+                --         default.get_token = function() return '' end
+                --         return default
+                --     end
+                -- }
+              },
+            },
+          },
         },
       },
     },
