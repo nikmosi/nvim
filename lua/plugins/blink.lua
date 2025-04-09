f47de01efd97dfd233f1be0d0ef1ca92e5eee476
nikmosi <i@nikmosi.ru>
2025-04-10 01:27:28 +0700

    chore(repository): remove comments

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -12,18 +12,6 @@ return {
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
-    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-    -- 'super-tab' for mappings similar to vscode (tab to accept)
-    -- 'enter' for enter to accept
-    -- 'none' for no mappings
-    --
-    -- All presets have the following mappings:
-    -- C-space: Open menu or open docs if already open
-    -- C-n/C-p or Up/Down: Select next/previous item
-    -- C-e: Hide menu
-    -- C-k: Toggle signature help (if signature.enabled = true)
-    --
-    -- See :h blink-cmp-config-keymap for defining your own keymap
     keymap = { preset = "super-tab" },
 
     appearance = {
@@ -37,88 +25,39 @@ return {
         lazydev = {
           name = "LazyDev",
           module = "lazydev.integrations.blink",
-          -- make lazydev completions top priority (see `:h blink.cmp`)
+
           score_offset = 100,
         },
         lsp = {
           name = "SNIPPETS",
           module = "blink.cmp.sources.snippets",
-          opts = {}, -- Passed to the source directly, varies by source
+          opts = {},
 
-          --- NOTE: All of these options may be functions to get dynamic behavior
-          --- See the type definitions for more information
-          enabled = true, -- Whether or not to enable the provider
-          async = false, -- Whether we should wait for the provider to return before showing the completions
-          timeout_ms = 2000, -- How long to wait for the provider to return before showing completions and treating it as asynchronous
-          transform_items = nil, -- Function to transform the items before they're returned
-          should_show_items = true, -- Whether or not to show the items
-          max_items = nil, -- Maximum number of items to display in the menu
-          min_keyword_length = 0, -- Minimum number of characters in the keyword to trigger the provider
-          -- If this provider returns 0 items, it will fallback to these providers.
-          -- If multiple providers fallback to the same provider, all of the providers must return 0 items for it to fallback
+          enabled = true,
+          async = false,
+          timeout_ms = 2000,
+          transform_items = nil,
+          should_show_items = true,
+          max_items = nil,
+          min_keyword_length = 0,
+
           fallbacks = {},
-          score_offset = 10, -- Boost/penalize the score of the items
-          override = nil, -- Override the source's functions
+          score_offset = 10,
+          override = nil,
         },
 
         git = {
           module = "blink-cmp-git",
           name = "Git",
-          -- only enable this source when filetype is gitcommit, markdown, or 'octo'
+
           enabled = function() return vim.tbl_contains({ "octo", "gitcommit", "markdown" }, vim.bo.filetype) end,
           --- @module 'blink-cmp-git'
           --- @type blink-cmp-git.Options
           opts = {
-            commit = {
-              -- You may want to customize when it should be enabled
-              -- The default will enable this when `git` is found and `cwd` is in a git repository
-              -- enable = function() end
-              -- You may want to change the triggers
-              -- triggers = { ':' },
-            },
+            commit = {},
             git_centers = {
-              github = {
-                -- Those below have the same fields with `commit`
-                -- Those features will be enabled when `git` and `gh` (or `curl`) are found and
-                -- remote contains `github.com`
-                -- issue = {
-                --     get_token = function() return '' end,
-                -- },
-                -- pull_request = {
-                --     get_token = function() return '' end,
-                -- },
-                -- mention = {
-                --     get_token = function() return '' end,
-                --     get_documentation = function(item)
-                --         local default = require('blink-cmp-git.default.github')
-                --             .mention.get_documentation(item)
-                --         default.get_token = function() return '' end
-                --         return default
-                --     end
-                -- }
-              },
-              gitlab = {
-                -- Those below have the same fields with `commit`
-                -- Those features will be enabled when `git` and `glab` (or `curl`) are found and
-                -- remote contains `gitlab.com`
-                -- issue = {
-                --     get_token = function() return '' end,
-                -- },
-                -- NOTE:
-                -- Even for `gitlab`, you should use `pull_request` rather than`merge_request`
-                -- pull_request = {
-                --     get_token = function() return '' end,
-                -- },
-                -- mention = {
-                --     get_token = function() return '' end,
-                --     get_documentation = function(item)
-                --         local default = require('blink-cmp-git.default.gitlab')
-                --            .mention.get_documentation(item)
-                --         default.get_token = function() return '' end
-                --         return default
-                --     end
-                -- }
-              },
+              github = {},
+              gitlab = {},
             },
           },
         },
--- lua/plugins/focus.lua
+++ lua/plugins/focus.lua
@@ -3,29 +3,29 @@ return {
   event = "VeryLazy",
   version = false,
   opts = {
-    enable = true, -- Enable module
-    commands = true, -- Create Focus commands
+    enable = true,
+    commands = true,
     autoresize = {
-      enable = false, -- Enable or disable auto-resizing of splits
+      enable = false,
     },
     split = {
-      bufnew = false, -- Create blank buffer for new split windows
-      tmux = false, -- Create tmux splits instead of neovim splits
+      bufnew = false,
+      tmux = false,
     },
     ui = {
-      number = false, -- Display line numbers in the focussed window only
-      relativenumber = false, -- Display relative line numbers in the focussed window only
-      hybridnumber = false, -- Display hybrid line numbers in the focussed window only
-      absolutenumber_unfocussed = false, -- Preserve absolute numbers in the unfocussed windows
+      number = false,
+      relativenumber = false,
+      hybridnumber = false,
+      absolutenumber_unfocussed = false,
 
-      cursorline = true, -- Display a cursorline in the focussed window only
-      cursorcolumn = false, -- Display cursorcolumn in the focussed window only
+      cursorline = true,
+      cursorcolumn = false,
       colorcolumn = {
-        enable = false, -- Display colorcolumn in the foccused window only
-        list = "+1", -- Set the comma-saperated list for the colorcolumn
+        enable = false,
+        list = "+1",
       },
-      signcolumn = false, -- Display signcolumn in the focussed window only
-      winhighlight = false, -- Auto highlighting for focussed/unfocussed windows
+      signcolumn = false,
+      winhighlight = false,
     },
   },
 }
