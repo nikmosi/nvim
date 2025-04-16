2ca47c9d74ef6f0f6537cb53868319b0036c614a
nikmosi <i@nikmosi.ru>
2025-04-16 21:00:32 +0700

    fix(todo): add snacks as dependencies

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -1,20 +1,17 @@
 return {
   "folke/todo-comments.nvim",
-  event = "BufRead",
+  event = "VeryLazy",
+  dependencies = { "folke/snacks.nvim" },
   opts = {},
   keys = {
     {
       "<leader>st",
-      function()
-        Snacks.picker.todo_comments()
-      end,
+      function() Snacks.picker.todo_comments() end,
       desc = "Todo",
     },
     {
       "<leader>sT",
-      function()
-        Snacks.picker.todo_comments { keywords = { "TODO", "FIX", "FIXME" } }
-      end,
+      function() Snacks.picker.todo_comments { keywords = { "TODO", "FIX", "FIXME" } } end,
       desc = "Todo/Fix/Fixme",
     },
   },
