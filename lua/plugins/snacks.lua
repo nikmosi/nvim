2d923f7f61a89e693a4a5e95cb250954dbf637c1
nikmosi <i@nikmosi.ru>
2025-04-30 01:48:47 +0700

    chore(snacks): disable image

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -15,7 +15,7 @@ return {
     scope = { enabled = true },
     toggle = { enabled = true },
     words = { enabled = true },
-    image = { enabled = true },
+    image = { enabled = false, doc = { inline = false } },
 
     dashboard = require "config.snacks.dashboard",
     indent = require "config.snacks.indent",
