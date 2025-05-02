199bd84f5ce22df79bd850daaec5c64746067f40
nikmosi <i@nikmosi.ru>
2025-05-03 00:23:19 +0700

    feat(auto-format): add nginxfmt

--- lua/plugins/auto-format.lua
+++ lua/plugins/auto-format.lua
@@ -10,6 +10,7 @@ return {
         lua = { "stylua" },
         fish = { "fish_indent" },
         python = { "ruff_fix", "ruff_format", "ruff_organize_import" },
+        nginx = { "nginxfmt" },
       },
       default_format_opts = {
         lsp_format = "fallback",
