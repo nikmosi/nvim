966c721d9129685ba5fd7bd3e9de9c25fde8ff04
nikmosi <i@nikmosi.ru>
2025-11-18 22:14:38 +0700

    fix(modicator): startup blink

--- lua/plugins/medicator.lua
+++ lua/plugins/medicator.lua
@@ -1,5 +1,6 @@
 return {
   "mawkler/modicator.nvim",
+  event = "VeryLazy",
   dependencies = "folke/tokyonight.nvim", -- Add your colorscheme plugin here
   init = function()
     -- These are required for Modicator to work
