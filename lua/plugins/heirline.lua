ba1a1b015681d4ca2018a7f3f92d9622b22516d8
nikmosi <i@nikmosi.ru>
2026-01-29 14:53:15 +0700

    feat: use heirline for tabline

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -1,10 +1,98 @@
 return {
   "rebelot/heirline.nvim",
   dependencies = { "Zeioth/heirline-components.nvim" },
-  event = "UIEnter", -- OK to lazy-load here. :contentReference[oaicite:3]{index=3}
+  event = "UIEnter",
+  keys = {
+    { "L", "<cmd>bnext<cr>", desc = "Next buffer" },
+    { "H", "<cmd>bprev<cr>", desc = "Previous buffer" },
+  },
   opts = function()
     local lib = require "heirline-components.all"
     local has_statuscol = vim.fn.has "nvim-0.10" == 1
+    local utils = require "heirline.utils"
+
+    local TablineBufnode = {
+      init = function(self)
+        self.filename = vim.api.nvim_buf_get_name(self.bufnr)
+        self.is_modified = vim.api.nvim_get_option_value("modified", { buf = self.bufnr })
+        self.is_active = vim.api.nvim_get_current_buf() == self.bufnr
+      end,
+      -- Buffer info part (Switch on left click, Close on right click)
+      {
+        on_click = {
+          callback = function(_, minwid, _, button)
+            if button == "l" then
+              vim.api.nvim_win_set_buf(0, minwid)
+            elseif button == "r" then
+              if _G.Snacks then
+                _G.Snacks.bufdelete(minwid)
+              else
+                vim.api.nvim_buf_delete(minwid, { force = false })
+              end
+              vim.cmd.redrawtabline()
+            end
+          end,
+          minwid = function(self) return self.bufnr end,
+          name = "heirline_tabline_buffer_callback",
+        },
+        {
+          provider = "｜",
+          hl = function(self) return { fg = self.is_modified and "yellow" or "green" } end,
+        },
+        {
+          init = function(self)
+            local extension = vim.fn.fnamemodify(self.filename, ":e")
+            self.icon, self.icon_color =
+              require("nvim-web-devicons").get_icon_color(self.filename, extension, { default = true })
+          end,
+          provider = function(self) return self.icon and (self.icon .. " ") end,
+          hl = function(self) return { fg = self.icon_color } end,
+        },
+        {
+          provider = function(self) return self.is_modified and "● " or "" end,
+          hl = { fg = "yellow" },
+        },
+        {
+          provider = function(self) return self.bufnr .. ": " end,
+        },
+        {
+          provider = function(self)
+            return self.filename == "" and "[No Name]" or vim.fn.fnamemodify(self.filename, ":t") .. " "
+          end,
+          hl = function(self)
+            if self.is_active then
+              return { bold = true, fg = utils.get_highlight("Normal").fg }
+            else
+              return { fg = utils.get_highlight("Comment").fg }
+            end
+          end,
+        },
+      },
+      -- Close button part (Close on left click)
+      {
+        provider = "󰅖 ",
+        hl = { fg = "gray" },
+        on_click = {
+          callback = function(_, minwid)
+            if _G.Snacks then
+              _G.Snacks.bufdelete(minwid)
+            else
+              vim.api.nvim_buf_delete(minwid, { force = false })
+            end
+            vim.cmd.redrawtabline()
+          end,
+          minwid = function(self) return self.bufnr end,
+          name = "heirline_tabline_close_buffer_callback",
+        },
+      },
+      { provider = " " },
+    }
+
+    local BufferLine = utils.make_buflist(
+      TablineBufnode,
+      { provider = "", hl = { fg = "gray" } }, -- left truncation
+      { provider = "", hl = { fg = "gray" } } -- right truncation
+    )
 
     return {
       -- note: this only matters if you define a winbar
@@ -27,8 +115,7 @@ return {
       },
 
       tabline = {
-        lib.component.tabline_conditional_padding(),
-        lib.component.tabline_buffers(),
+        BufferLine,
         lib.component.fill { hl = { bg = "tabline_bg" } },
         lib.component.tabline_tabpages(),
       },
@@ -65,11 +152,5 @@ return {
     hc.init.subscribe_to_events()
     heirline.load_colors(hc.hl.get_colors())
     heirline.setup(opts)
-
-    -- show tabline only when >1 listed buffer
-    -- vim.api.nvim_create_autocmd("User", {
-    --   pattern = "HeirlineComponentsTablineBuffersUpdated",
-    --   callback = function() vim.o.showtabline = (#vim.t.bufs > 1) and 2 or 1 end,
-    -- }) -- event documented in plugin README. :contentReference[oaicite:6]{index=6}
   end,
 }
